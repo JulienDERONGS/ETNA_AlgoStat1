0a6bf8687d936c4853f44bb75158059b0ad38113
JulienDERONGS <julien.derongs@masasgoup.net>
2018-01-31 09:56:27 +0100

    ProjPath added to config class

--- include/Config.php
+++ include/Config.php
@@ -5,6 +5,7 @@
 class                       Config extends SingletonFactory
 {
   private static            $path = "/var/www/html/algostat1";
+  private static            $proj_path = "/algostat1";
   private static            $ip = "localhost";
   private static            $dbname ="algostat_db";
   private static            $username = "root";
@@ -13,6 +14,7 @@ class                       Config extends SingletonFactory
   function __destruct()
   {
     static::$path = NULL;
+    static::$proj_path = NULL;
     static::$ip = NULL;
     static::$dbname = NULL;
     static::$username = NULL;
@@ -20,6 +22,7 @@ class                       Config extends SingletonFactory
   }
 
   function getPath()        {return (static::$path);}
+  function getProjPath()    {return (static::$proj_path);}
   function getIP()          {return (static::$ip);}
   function getDBname()      {return (static::$dbname);}
   function getUsername()    {return (static::$username);}
--- index.php
+++ index.php
@@ -27,7 +27,7 @@
 
   <body>
     <div class="algo_form">
-      <form action="/algostat1/process.php" method="post">
+      <form action="<?php echo $config->getProjPath() ?>/process.php" method="post">
         Sort type :</br>
         <select name="type">
           <option value="insertion">Insertion</option>
